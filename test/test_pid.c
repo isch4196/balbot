b43aa94a50d9d1e3e3368d0528a7bb681aa6b991
isch <isch4196@colorado.edu>
2023-09-15 09:24:06 -0700

    Updated more tests, changed input parameter order to match function call

--- test/test_pid.c
+++ test/test_pid.c
@@ -13,7 +13,6 @@ void tearDown(void)
 {
 }
 
-
 void test_pid_kp(void)
 {
     PID_TypeDef TPID;
@@ -29,17 +28,57 @@ void test_pid_kp(void)
     usleep(1000);
     angle = 1;
     PID_Compute(&TPID);
-    TEST_ASSERT_EQUAL(pid_out, -5); 
+    TEST_ASSERT_EQUAL(-5, pid_out); 
 }
 
 void test_pid_kd(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement kd");
+   TEST_IGNORE_MESSAGE("Need to Implement kd");
 }
 
 void test_pid_ki(void)
 {
     TEST_IGNORE_MESSAGE("Need to Implement ki");
+}
+
+void test_pid_positive_to_negative(void)
+{
+    // if we read a positive angle, we want to move motors in opposite dir,
+    // hence expect a negative num
+    PID_TypeDef TPID;
+    double angle, pid_out, angle_set_point;
+
+    angle_set_point = 0;
+    PID(&TPID, &angle, &pid_out, &angle_set_point, 5, 0, 0, _PID_P_ON_E, _PID_CD_DIRECT);
+    
+    PID_SetMode(&TPID, _PID_MODE_AUTOMATIC);
+    PID_SetSampleTime(&TPID, 1); // set sample time to 1ms for testing
+    PID_SetOutputLimits(&TPID, -100, 100); // need to update
+
+    usleep(1000);
+    angle = 1;
+    PID_Compute(&TPID);
+    TEST_ASSERT_LESS_THAN(0, pid_out);
+}
+
+void test_pid_negative_to_positive(void)
+{
+    // if we read a negative angle, we want to move motors in opposite dir,
+    // hence expect a positive num
+    PID_TypeDef TPID;
+    double angle, pid_out, angle_set_point;
+
+    angle_set_point = 0;
+    PID(&TPID, &angle, &pid_out, &angle_set_point, 5, 0, 0, _PID_P_ON_E, _PID_CD_DIRECT);
+    
+    PID_SetMode(&TPID, _PID_MODE_AUTOMATIC);
+    PID_SetSampleTime(&TPID, 1); // set sample time to 1ms for testing
+    PID_SetOutputLimits(&TPID, -100, 100); // need to update
+
+    usleep(1000);
+    angle = -1;
+    PID_Compute(&TPID);
+    TEST_ASSERT_GREATER_THAN(0, pid_out);
 }
 
 #endif // TEST
