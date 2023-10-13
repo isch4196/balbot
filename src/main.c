75cb1253ba07c9f1f4e414b98caa77783d56a874
isch <isch4196@colorado.edu>
2023-10-13 16:10:11 -0700

    Testing

--- src/main.c
+++ src/main.c
@@ -68,8 +68,10 @@ int main(int argc, char *argv[])
 
     pid_t pid = fork();
     if (0 == pid) { // child
-	execl("/bin/libcamera-vid", "libcamera-vid", "-n", "-t", "0", "--inline", "--width", "640", "--height", "480", "--framerate", "30", "-o", "udp://000.0.00.00:3900", (char*)0);
+	int ret = execl("/bin/libcamera-vid", "libcamera-vid", "-n", "-t", "0", "--inline", "--width", "640", "--height", "480", "--framerate", "30", "-o", "udp://192.168.1.75:3900", (char*)0);
+	printf("FORK CHILD: %d\n", ret);
     } else if (pid > 0) { // parent
+	printf("FORK PARENT\n");
 	goto parent_process;
     } else {
 	perror("fork error");
